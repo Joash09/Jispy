07c1b72c43bfd9114123c8b28320037003b48c46
Joash <joash.n09@gmail.com>
2022-04-17 16:03:54 +0200

    Can now handle basic errors. Chapter 8

--- prompt.c
+++ prompt.c
@@ -3,14 +3,27 @@
 #include <stdlib.h>
 #include <editline.h>
 #include <string.h>
+#include <math.h>
 
 /* Included libraries */
 #include "mpc.h"
 
-float eval(mpc_ast_t* t);
-float eval_op(float x, float y, char* op);
+typedef struct {
+    int type;
+    float value;
+    int error;
+} lval;
 
-static char input[2048];
+enum LVAL_TYPE { LVAL_NUM, LVAL_ERROR };
+enum LERR_TYPE { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+lval eval(mpc_ast_t* t);
+lval eval_op(lval x, lval y, char* op);
+lval lval_num(float num);
+lval lval_error(int error);
+void lval_print(lval p);
+
+static char input[2048]; // Global input buffer
 
 int main(int argc, char** argv) {
 
@@ -23,7 +36,7 @@ int main(int argc, char** argv) {
         // Define Grammar
         mpca_lang(MPCA_LANG_DEFAULT, " \
 number: /-?[0-9]+(\\.[0-9]+)?/ ; \
-operator: '+' | '-' | '*' | '/' ; \
+operator: '+' | '-' | '*' | '/' | '^'; \
 expression: <number> | '(' <operator> <expression>+ ')' ; \
 lisps: /^/ <operator> <expression>+ /$/ ; \
         ",
@@ -41,8 +54,8 @@ lisps: /^/ <operator> <expression>+ /$/ ; \
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lisps, &r)) {
 
-            float result = eval(r.output);
-            printf("%f\n", result);
+            lval result = eval(r.output);
+            lval_print(result);
             mpc_ast_delete(r.output);
 
         } else {
@@ -64,16 +77,16 @@ lisps: /^/ <operator> <expression>+ /$/ ; \
 // The first child is tagged regex
 // The second child is tagged operator (i.e. +)
 // Third and Fourth child is tagged expression | number
-float eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
 
     if (strstr(t->tag, "number")) {
-        return atof(t->contents);
+        return lval_num(atof(t->contents));
     }
 
-    // else is this lisp
+    // else is this has operators and expressions
     char *op = t->children[1]->contents;
 
-    float x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     while(strstr(t->children[i]->tag, "expression")) {
@@ -84,10 +97,54 @@ float eval(mpc_ast_t* t) {
     return x;
 }
 
-float eval_op(float x, float y, char* op) {
-    if (strstr(op, "+")) { return x + y; }
-    if (strstr(op, "-")) { return x - y; }
-    if (strstr(op, "*")) { return x * y; }
-    if (strstr(op, "/")) { return x / y; }
-    return 0;
+lval eval_op(lval x, lval y, char* op) {
+
+    if(x.type == LVAL_ERROR) { return x; }
+    if(y.type == LVAL_ERROR) { return y; }
+
+    if (strstr(op, "+")) { return lval_num(x.value + y.value); }
+    if (strstr(op, "-")) { return lval_num(x.value - y.value); }
+    if (strstr(op, "*")) { return lval_num(x.value * y.value); }
+    if (strstr(op, "/")) { return y.value == 0 ? lval_error(LERR_DIV_ZERO) : lval_num(x.value/y.value); }
+    if (strstr(op, "^")) { return lval_num(pow(x.value, y.value)); }
+    // if (strstr(op, "%")) { return modff(x, y); }
+    //
+    return lval_error(LERR_BAD_OP);
+}
+
+lval lval_num(float num) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.value = num;
+    return v;
+}
+
+lval lval_error(int err) {
+    lval v;
+    v.type = LVAL_ERROR;
+    v.error = err;
+    return v;
+}
+
+void lval_print(lval p) {
+    switch(p.type) {
+
+        case LVAL_NUM: {
+            printf("%f\n", p.value);
+        }
+        break;
+        case LVAL_ERROR: {
+            switch(p.error) {
+            case LERR_DIV_ZERO: {
+              printf("Cannot divide by zero\n");
+            } break;
+            case LERR_BAD_NUM: {
+              printf("Cannot convert number\n");
+            } break;
+            case LERR_BAD_OP: {
+              printf("Unsupported operator\n");
+            } break;
+            }
+        }
+    }
 }
